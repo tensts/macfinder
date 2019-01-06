9597ebb32787d51622f56661cff0471fd222aa70
tensts <sprawdztenstrzal@gmail.com>
2019-01-06 11:48:20 +0100

    symlink issue fix

--- macfinder.py
+++ macfinder.py
@@ -2,8 +2,9 @@
 
 import csv
 import sys
+import os
 
-DB_NAME="macaddress.io-db.csv"
+DB_NAME=os.path.join(os.path.dirname(os.path.realpath(__file__)),"macaddress.io-db.csv")
 
 if len(sys.argv) != 2:
     sys.exit("usage ./macfinder MACADDRESS")
@@ -17,7 +18,7 @@ with open(DB_NAME) as f:
     reader = csv.DictReader(f)
     try:
         for row in reader:
-            vendorAddress = row['oui'].replace(":","").strip().upper()[:6]
+            vendorAddress = row['oui'].replace(":","").strip().upper()
 
             if macaddress.startswith(vendorAddress):
                 print "[+] FOUND: %s" % row
